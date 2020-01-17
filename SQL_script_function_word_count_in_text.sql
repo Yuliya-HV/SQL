7d5ad3ea5933183195cb776450b0fe7567b8da0e
Yuliya-H <49458989+Yuliya-HV@users.noreply.github.com>
2020-01-17 16:19:44 +0000

    Add files via upload
    This function is borrowed from somewhere in Internet.

---
+++ SQL_script_function_word_count_in_text.sql
@@ -0,0 +1,30 @@
+FUNCTION `WORD_COUNT`(str TEXT) 
+RETURNS int(11)
+
+BEGIN
+         
+	DECLARE wordCnt, idx, maxIdx INT DEFAULT 0;
+         	
+	DECLARE currChar, prevChar BOOL DEFAULT 0;
+         
+	SET maxIdx=char_length(str);
+         
+	WHILE idx < maxIdx DO
+             
+		SET currChar=SUBSTRING(str, idx, 1) RLIKE '[[:alnum:]]';
+             
+		IF NOT prevChar AND currChar THEN
+                 
+			SET wordCnt=wordCnt+1;
+             
+		END IF;
+             
+		SET prevChar=currChar;
+             
+		SET idx=idx+1;
+         
+	END WHILE;
+         
+	RETURN wordCnt;
+
+END
