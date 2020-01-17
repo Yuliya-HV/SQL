a0e1af1561b15062ff9ac24a2637c2269d51b615
Yuliya-H <49458989+Yuliya-HV@users.noreply.github.com>
2020-01-17 16:16:16 +0000

    Add files via upload
    Procedure to update Null values in MySQL database tables.

---
+++ SQL_script_procedure_update_values_any_table.sql
@@ -0,0 +1,34 @@
+//This procedure update table t_name (pass as a parameter) by replacing Null values with empty strings.
+
+PROCEDURE `PROC_REPLACE_NULL`(IN t_name CHAR(20))
+	
+BEGIN
+	SET @table = t_name;    
+    
+		SET @upd1 = CONCAT('UPDATE ', @table, ' SET snippet = \'\' WHERE snippet is Null');
+    
+		PREPARE upd11 FROM @upd1;
+    
+		EXECUTE upd11;
+    
+		DEALLOCATE PREPARE upd11;
+    
+    
+		SET @upd2 = CONCAT('UPDATE ', @table, ' SET lead_paragraph = \'\' WHERE lead_paragraph is Null');
+    
+		PREPARE upd22 FROM @upd2;
+    
+		EXECUTE upd22;
+    
+		DEALLOCATE PREPARE upd22;
+    
+    
+		SET @upd3 = CONCAT('UPDATE ', @table, ' SET abstract = \'\' WHERE abstract is Null');
+    
+		PREPARE upd33 FROM @upd3;
+    
+		EXECUTE upd33;
+    
+		DEALLOCATE PREPARE upd33;
+	
+END
